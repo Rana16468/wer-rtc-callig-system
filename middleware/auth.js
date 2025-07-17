becc4873740eb9e733d1640ca8019f1e363b1704
Rana16468 <rana16-468@diu.edu.bd>
2025-07-18 03:55:54 +0600

    sending the env file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const authenticateToken = async (req, res, next) => {
   try {
